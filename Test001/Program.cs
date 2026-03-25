70c3c1f3a7d3c6b57603226a5e43fbc7696d65c9
Machinessssss <ge.amateur@gmail.com>
2026-03-25 17:46:26 +0800

    添加项目文件。

---
+++ Test001/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test001
+{
+    class LogManager
+    {
+        public static string LogPath;
+
+        // 静态构造：只执行一次
+        static LogManager()
+        {
+            // 拼接日志路径 = 程序目录 + log.txt
+            LogPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log.txt");
+            Console.WriteLine("日志文件路径：" + LogPath);
+        }
+
+        // 写日志
+        public static void WriteLog(string message)
+        {
+            // 追加写入文件
+            File.AppendAllText(LogPath, $"{DateTime.Now}: {message}\n");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            LogManager.WriteLog("程序启动啦！");
+            LogManager.WriteLog("用户登录成功！");
+            Console.WriteLine("日志已写入！");
+            Console.WriteLine("请按任意键退出...");
+            Console.ReadKey();
+        }
+    }
+}
